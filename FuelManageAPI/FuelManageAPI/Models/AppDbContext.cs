99dc171eab65467f47e73f3ac33909fbc51c3ee0
Geisson Oliveira <geissonlucaso@gmail.com>
2025-03-20 20:31:16 -0300

    Configure EntityFramework.

---
+++ FuelManageAPI/FuelManageAPI/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FuelManageAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+    }
+}
--- FuelManageAPI/FuelManageAPI/Program.cs
+++ FuelManageAPI/FuelManageAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using FuelManageAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace FuelManageAPI
 {
     public class Program
@@ -8,8 +11,12 @@ namespace FuelManageAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
