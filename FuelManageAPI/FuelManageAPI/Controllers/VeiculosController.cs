0c86937bceafb3d4b6df61d28184eb020d1a440c
Geisson Oliveira <geissonlucaso@gmail.com>
2025-03-21 10:38:06 -0300

    Create Veiculo Controller with GetAll endpoint.

---
+++ FuelManageAPI/FuelManageAPI/Controllers/VeiculosController.cs
@@ -0,0 +1,27 @@
+﻿using FuelManageAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuelManageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Veiculos.ToListAsync();
+
+            return Ok(model);
+        }
+    }
+}
